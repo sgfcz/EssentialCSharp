fc8062e9fc205e9da46cf5db0b5913d997457864
sgfcz <coderxiao@sohu.com>
2022-08-06 17:00:04 +0800

    first

---
+++ HeyYou/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+class HeyYou
+{
+    static void Main()
+    {
+        string firstName, lastName;
+        WriteLine("Hey！You");
+
+        Write("Enter you frist name: ");
+        firstName = ReadLine();
+
+        Write("Enter you last name: ");
+        lastName = ReadLine();
+
+        WriteLine($"Your full name is {firstName} {lastName}.");
+    }
+}
